e979385f93acefd5d2424d30d3e7671c0efaf27e
hung3a8 <hung3a8@gmail.com>
2022-12-15 13:15:31 +0100

    Add base_handler

---
+++ bridge/models/__init__.py
@@ -0,0 +1,23 @@
+import json
+
+from peewee import MySQLDatabase, Model
+from playhouse.reflection import generate_models
+
+database = MySQLDatabase(
+    **json.load(open('config.json'))['database']
+)
+
+database.connect()
+
+models = generate_models(database)
+
+User: Model = models['user']
+Class: Model = models['class']
+Judge: Model = models['judge']
+Problem: Model = models['problem']
+Submission: Model = models['submission']
+
+# Runtime
+Judge: Model = models['judge']
+Language: Model = models['language']
+RuntimeVersion: Model = models['runtime_version']
